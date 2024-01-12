09926fb7d88d47c285fb38c075cd76aca0fa586c
lifechang <1164844313@qq.com>
2024-01-12 21:19:54 +0800

    save

--- UI/packages/MyForm/src/Row.vue
+++ UI/packages/MyForm/src/Row.vue
@@ -2,25 +2,25 @@
   <div class="row-container">
     <div class="row-box">
       <!-- 拿到每一行 -->
-      {{ CurProps }}
       <el-row v-for="(items, index) in RowList.formList" :key="index" :gutter="RowList.gutter">
         <!-- 拿到每一行每一个 -->
         <el-col v-bind="RowList.cols" v-for="(item, itemIndex) in items" :key="itemIndex" :span="setSpan(items, item, RowData)">
-          {{  index }} - 99
-
           <template v-if="getShow(RowData, item)">
             <slot v-if="item.el === 'custom' && !item.label" :name="item.parentValue ? `${[item.parentValue]}.${[item.value]}` : item.value" :data="RowData" :item="item" />
             <el-form-item v-bind="{ ...item, ...item.props }" v-else-if="item.el === 'towLevel'" :prop="item.value">
-              <!-- {{ CurProps }} -->
+              <!-- {{ RowData }}---{{ item }} -->
               <!-- {{  item.multiple ? item.parentValue ? RowData[item.parentValue][itemIndex][item.value]?.length : RowData[item.value]?.length : 1  }} -->
-              <Row v-for="(even, evenIndex) in item.multiple ? item.parentValue ? RowData[item.parentValue][itemIndex][item.value]?.length : RowData[item.value]?.length : 1" :key="evenIndex" :RowList="isTowLevel(item)" :EvenIndex="evenIndex" :CurProps="item.el === 'towLevel' ?  setaaa(item, itemIndex, evenIndex) : item.value" :RowData="RowData">
-                <template v-for="slot in Object.keys($scopedSlots)" #[slot]="scope">
-                  <slot :name="slot" :data="scope" :index="evenIndex" :parentItem="isTowLevel(item)"></slot>
-                </template>
-              </Row>
-              <i v-if="item.multiple" :style="{ marginLeft: $attrs['label-width'] ? $attrs['label-width'] : '5rem' }" @click="addItemList(item, RowData, itemIndex)" class="el-icon-plus myIcon add"></i>
+              <template v-for="(even, evenIndex) in item.multiple ? item.parentValue ? RowData[item.parentValue][itemIndex][item.value]?.length : RowData[item.value]?.length : 1">
+                <Row :key="evenIndex" :RowList="isTowLevel(item)" :EvenIndex="evenIndex" :CurProps="setaaa(item, evenIndex)" :RowData="RowData">
+                  <template v-for="slot in Object.keys($scopedSlots)" #[slot]="scope">
+                    <slot :name="slot" :data="scope" :index="evenIndex" :parentItem="isTowLevel(item)"></slot>
+                  </template>
+                </Row>
+              </template>
+              <i v-if="item.multiple" :style="{ marginLeft: $attrs['label-width'] ? $attrs['label-width'] : '5rem' }" @click="addItemList(item, RowData)" class="el-icon-plus myIcon add"></i>
             </el-form-item>
             <el-form-item v-bind="{ ...item, ...item.props }" :prop="item.parentValue ? `${CurProps}.${[item.value]}` : item.value" v-else>
+              ----{{ item.parentValue ? `${CurProps}.${[item.value]}` : item.value }}
               <slot v-if="item.el === 'custom' && item.label" :name="item.parentValue ? `${[item.parentValue]}.${[item.value]}` : item.value" :data="RowData" :item="item" />
               <SearchFormItem v-else :column="handle(item)" :EvenIndex="EvenIndex" :CurProps="item.parentValue ? `${CurProps}.${[item.value]}` : item.value" :search-param="RowData"></SearchFormItem>
             </el-form-item>
@@ -106,39 +106,47 @@ export default {
     // console.log(this.RowList, this.EvenIndex, this.RowData);
   },
   methods: {
-    setaaa(item, index, eve) {
-      console.log(item, index, eve, this.CurProps, 'qqq');
-      console.log(item.parentValue ? `${this.CurProps}.${item.value}.${eve}` : `${item.value}.${index}`)
-      return item.parentValue ? `${this.CurProps}.${item.value}.${eve}` : `${item.value}.${index}`;
+    setaaa(item, eve) {
+      // console.log(item, index, eve, this.CurProps, this.RowData, 'qqq');
+      // console.log(item.parentValue ? `${this.CurProps}.${item.value}.${eve}` : `${item.value}.${eve}`)
+      if (item.formList) {
+      return item.parentValue ? `${this.CurProps}.${item.value}.${eve}` : `${item.value}.${eve}`;
+      } else {
+        return item.value
+      }
     },
-    addItemList(item, data, evenIndex) {
-      console.log(item, data, evenIndex, this.CurProps);
+    setData(item) {
+      console.log(item, this.CurProps, this.RowData);
+      let newObj = {};
+      for (let v of item.formList) {
+        for (let v2 of v) {
+          if (v2.formList) {
+            _.merge(newObj, { [v2.value]: Array(this.RowData[item.value][0][v2.value].length).fill([this.setData(v2)]) });
+          } else {
+          if (v2.value.includes(".")) {
+            _.merge(
+              newObj,
+              v2.value.split(".").reduceRight((obj, next) => ({ [next]: obj }), v2.el === 'switch' ? true : undefined)
+            );
+          } else {
+            _.merge(newObj, { [v2.value]: v2.el === 'switch' ? true : '' });
+          }
+          }
+        }
+      }
+      return newObj
+    },
+    addItemList(item, data) {
       let value = this.CurProps.split('.').reduce((acc, cur) => acc && acc[cur], data);
-      console.log(value, 'aa')
+      let aaa = this.setData(item)
+      console.log(aaa, 'aaa');
       if (item.parentValue) {
-        console.log(111)
         value[item.value].push({})
       } else {
-        data[item.value].push({})
+        data[item.value].push(aaa)
         // this.$set(data[item.value], evenIndex, {})
-        console.log(22)
       }
-      console.log(data)
-      // 获取新增数据结构
-      // let newObj = {};
-      // for (let v of item.formList) {
-      //   for (let v2 of v) {
-      //     if (v2.value.includes(".")) {
-      //       _.merge(
-      //         newObj,
-      //         v2.value.split(".").reduceRight((obj, next) => ({ [next]: obj }), v2.el === 'switch' ? true : undefined)
-      //       );
-      //     } else {
-      //       _.merge(newObj, { [v2.value]: v2.el === 'switch' ? true : '' });
-      //     }
-      //   }
-      // }
-      // // Vue.observable变为响应式对象
+      console.log(data, value);
       // if (item.el === 'towLevel') {
       //   this.$set(data[item.value], evenIndex, Vue.observable(newObj));
       // } else {
